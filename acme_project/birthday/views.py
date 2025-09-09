01b79702b13f5a99c05fae2e446600ca1a6860ad
vodoleikin <vodoleikin@yandex.ru>
2025-09-09 17:21:15 +0300

    v3 09.09.2025. after lesson 9-3-1

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -5,6 +5,8 @@ from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 # Импортируем модель дней рождения.
 from .models import Birthday
+# Импортируем класс пагинатора.
+from django.core.paginator import Paginator
 
 
 # Добавим опциональный параметр pk.
@@ -39,11 +41,21 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
-    # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
+    # Получаем список всех объектов с сортировкой по id.
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 10)
+
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора. 
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст 
+    # объект страницы пагинатора
+    context = {'page_obj': page_obj}
+    return render(request, 'birthday/birthday_list.html', context) 
 
 
 def delete_birthday(request, pk):
@@ -61,3 +73,20 @@ def delete_birthday(request, pk):
         return redirect('birthday:list')
     # Если был получен GET-запрос — отображаем форму.
     return render(request, 'birthday/birthday.html', context)
+
+
+
+
+
+
+
+
+
+
+# ----------
+# def birthday_list(request):
+#     # Получаем все объекты модели Birthday из БД.
+#     birthdays = Birthday.objects.all()
+#     # Передаём их в контекст шаблона.
+#     context = {'birthdays': birthdays}
+#     return render(request, 'birthday/birthday_list.html', context)
