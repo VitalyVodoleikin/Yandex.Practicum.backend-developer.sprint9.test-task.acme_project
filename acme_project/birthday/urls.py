0d03e0b792daee3cb0159d693045256838ec70ad
vodoleikin <vodoleikin@yandex.ru>
2025-09-11 12:38:50 +0300

    v1 11.09.2025. lesson 9-3-3, CBV

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     # Работа с CBV
     path('', views.BirthdayCreateView.as_view(), name='create'),
     path('list/', views.BirthdayListView.as_view(), name='list'),
+    path('<int:pk>/', views.BirthdayDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.BirthdayUpdateView.as_view(), name='edit'),
     path('<int:pk>/delete/', views.BirthdayDeleteView.as_view(), name='delete'),
 ]
